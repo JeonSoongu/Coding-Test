094bbb13ac4459acf263b34306c3591b26887c17
JeonSoongu <soonguprog@gmail.com>
2021-08-30 00:36:21 +0900

    스킬트리

---
+++ 프로그래머스 레벨2/스킬트리.js
@@ -0,0 +1,18 @@
+const skill = "CBD";
+const skill_trees = ["BACDE", "CBADF", "AECB", "BDA"];
+
+function solution(skill, skill_trees) {
+  function isCorrect(n) {
+    let test = skill.split("");
+    for (var i = 0; i < n.length; i++) {
+      if (!skill.includes(n[i])) continue;
+      if (n[i] === test.shift()) continue;
+      return false;
+    }
+    return true;
+  }
+
+  return skill_trees.filter(isCorrect).length;
+}
+
+console.log(solution(skill, skill_trees));
