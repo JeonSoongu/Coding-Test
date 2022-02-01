c51ebd6a9592f077d08689f1896d6678a4348e1b
JeonSoongu <soonguprog@gmail.com>
2022-02-01 19:15:14 +0900

    실패율 코드 가독성 수정

--- 프로그래머스 레벨1/실패율.js
+++ 프로그래머스 레벨1/실패율.js
@@ -1,15 +1,11 @@
 function solution(N, stages) {
-  const failureRateArr = {};
+  const failureRateArr = [];
   let personNum = stages.length;
   for (let i = 1; i < N + 1; i++) {
     const failureNum = stages.filter((x) => x === i).length;
-    failureRateArr[i] = failureNum / personNum;
+    failureRateArr.push([i, failureNum / personNum]);
     personNum = personNum - failureNum;
   }
-  let sortobj = [];
-  for (let number in failureRateArr) {
-    sortobj.push([Number(number), failureRateArr[number]]);
-  }
-  sortobj.sort((a, b) => b[1] - a[1]);
+  failureRateArr.sort((a, b) => b[1] - a[1]);
   return sortobj.map((x) => x[0]);
 }
